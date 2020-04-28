1d9984b4746dd031010d8699bbfe2cfdfc56ea16
Weslley Zanirate <zanirate90@gmail.com>
2020-04-28 19:07:51 -0300

    ok

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,5 @@ import { Router } from 'express';
 import transactionRouter from './transaction.routes';
 
 const routes = Router();
-
 routes.use('/transactions', transactionRouter);
-
 export default routes;
